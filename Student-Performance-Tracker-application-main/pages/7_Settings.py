03469487cde4c0be22d86a5246da91826b53e4a7
gouransh1024 <gouransh1024@gmail.com>
2025-08-29 01:11:46 +0530

    11

--- Student-Performance-Tracker-application-main/pages/7_Settings.py
+++ Student-Performance-Tracker-application-main/pages/7_Settings.py
@@ -608,7 +608,7 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     st.markdown("**📧 Email Support**")
-    st.write("kratikjain121@gmail.com")
+    st.write("gouransh1024@gmail.com")
 
 with col2:
     st.markdown("**📖 Documentation**")  
